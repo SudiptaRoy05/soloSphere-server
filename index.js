1b27576374153ad710698a28a9bed341f0647490
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-12-23 23:58:16 +0600

    get api for update added

--- index.js
+++ index.js
@@ -54,6 +54,14 @@ async function run() {
       res.send(result);
     })
 
+    // get single data 
+    app.get('/job/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobsCollection.findOne(query);
+      res.send(result);
+    })
+
 
   } finally {
     // Ensures that the client will close when you finish/error
